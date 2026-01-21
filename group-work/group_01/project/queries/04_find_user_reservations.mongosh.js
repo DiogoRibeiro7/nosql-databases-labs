04b5009dcf5930b4652d588e913c2b5c8ba539da
João Rego <116601075+joaorego01@users.noreply.github.com>
2026-01-21 16:49:05 +0000

    feat: implement find user reservations query

--- group-work/group_01/project/queries/04_find_user_reservations.mongosh.js
+++ group-work/group_01/project/queries/04_find_user_reservations.mongosh.js
@@ -3,3 +3,7 @@
 // Usage: mongosh queries/04_find_user_reservations.mongosh.js
 
 db = db.getSiblingDB("group_01_flight_management_system_final");
+print("Finding user reservations:");
+db.reservations
+  .find({ userId: ObjectId("697102e5d31ee2696f40e8d9") })
+  .forEach((doc) => printjson(doc));
