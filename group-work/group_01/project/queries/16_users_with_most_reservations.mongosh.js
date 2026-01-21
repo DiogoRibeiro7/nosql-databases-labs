0671b60bbfabdd652b771c2cf243a3dc5f9dca40
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-01-21 19:05:18 +0000

    feat: implement users with most reservations query

--- group-work/group_01/project/queries/16_users_with_most_reservations.mongosh.js
+++ group-work/group_01/project/queries/16_users_with_most_reservations.mongosh.js
@@ -3,3 +3,34 @@
 // Usage: mongosh queries/16_users_with_most_reservations.mongosh.js
 
 db = db.getSiblingDB("group_01_flight_management_system_final");
+print("Users with the most reservations:");
+
+db.users
+  .aggregate([
+    {
+      $lookup: {
+        from: "reservations",
+        localField: "_id",
+        foreignField: "userId",
+        as: "reservations"
+      }
+    },
+    {
+      $addFields: {
+        reservationsCount: { $size: "$reservations" }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        name: 1,
+        email: 1,
+        reservationsCount: 1
+      }
+    },
+    {
+      $sort: { reservationsCount: -1 }
+    },
+    { $limit: 10 }
+  ])
+  .forEach((doc) => printjson(doc));
