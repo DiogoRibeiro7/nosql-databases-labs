48d167e868512e30d49d0bd0e34c144ea5d629d9
P <194640994+escolaP@users.noreply.github.com>
2026-01-21 21:35:42 +0000

    outra query

--- group-work/group_09/Project/queries/01_film_infos.mongosh.js
+++ group-work/group_09/Project/queries/01_film_infos.mongosh.js
@@ -1,81 +1,85 @@
 db = db.getSiblingDB("sakila");
 
-db.film
-    .aggregate([
-    { $lookup: {
-        from: "film_actor",
-        localField: "film_id",
-        foreignField: "film_id",
-        pipeline: [
-            {
-                $lookup: {
-                    from: "actor",
-                    localField: "actor_id",
-                    foreignField: "actor_id",
-                    pipeline: [
-                        {
-                            $project: { 
-                                _id: 0,
-                                actor_id: 0,
-                                last_update: 0
-                            } 
-                        }
-                    ],
-                    as: "nome"
-                }
-            }, 
-            { 
-                $project: { 
-                    _id: 0,
-                    film_id: 0,
-                    actor_id: 0,
-                    last_update: 0
-                } 
-            }
-        ],
-        as: "actor"
-    } },
-    { $lookup: {
-        from: "film_category",
-        localField: "film_id",
-        foreignField: "film_id",
-        pipeline: [
-            {
-                $lookup: {
-                    from: "category",
-                    localField: "category_id",
-                    foreignField: "category_id",
-                    pipeline: [
-                        {
-                            $project: { 
-                                _id: 0,
-                                category_id: 0,
-                                last_update: 0
-                            } 
-                        }
-                    ],
-                    as: "category"
-                },
-            },
-            { 
-                $project: { 
-                    _id: 0,
-                    film_id: 0,
-                    category_id: 0,
-                    last_update: 0
-                } 
-            }
-        ],
-        as: "category"
-    } },
-    { $project: {
-        _id: 0,
-        film_id: 0,
-        language_id: 0,
-        length: 0,
-        last_update: 0
-    } },
-    { $sort: { _id: 1} },
-    { $limit: 5 }
-])
-.forEach((doc) => printjson(doc));
+db.films.aggregate([
+	{
+		$lookup: {
+			from: "film_actor",
+			localField: "film_id",
+			foreignField: "film_id",
+			pipeline: [
+				{
+					$lookup: {
+						from: "actor",
+						localField: "actor_id",
+						foreignField: "actor_id",
+						pipeline: [
+							{
+								$project: {
+									_id: 0,
+									actor_id: 0,
+									last_update: 0
+								}
+							}
+						],
+						as: "nome"
+					}
+				},
+				{
+					$project: {
+						_id: 0,
+						film_id: 0,
+						actor_id: 0,
+						last_update: 0
+					}
+				}
+			],
+			as: "actor"
+		}
+	},
+	{
+		$lookup: {
+			from: "film_category",
+			localField: "film_id",
+			foreignField: "film_id",
+			pipeline: [
+				{
+					$lookup: {
+						from: "category",
+						localField: "category_id",
+						foreignField: "category_id",
+						pipeline: [
+							{
+								$project: {
+									_id: 0,
+									category_id: 0,
+									last_update: 0
+								}
+							}
+						],
+						as: "category"
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						film_id: 0,
+						category_id: 0,
+						last_update: 0
+					}
+				}
+			],
+			as: "category"
+		}
+	},
+	{
+		$project: {
+			_id: 0,
+			film_id: 0,
+			language_id: 0,
+			length: 0,
+			last_update: 0
+		}
+	},
+	{ $sort: { _id: 1 } },
+	{ $limit: 5 }
+]).forEach((doc) => printjson(doc));
---
+++ group-work/group_09/Project/queries/xx_all_customer_rentals.mongosh.js
@@ -0,0 +1,82 @@
+// criar indexes
+db.rental.createIndex({ customer_id: 1 });
+db.inventory.createIndex({ inventory_id: 1 });
+db.film.createIndex({ film_id: 1 });
+db.staff.createIndex({ staff_id: 1 });
+
+db.customer.aggregate([
+	{
+		$lookup: {
+			from: "rental",
+			localField: "customer_id",
+			foreignField: "customer_id",
+			as: "rentals"
+		}
+	},
+	{ $unwind: "$rentals" },
+	{
+		$lookup: {
+			from: "inventory",
+			localField: "rentals.inventory_id",
+			foreignField: "inventory_id",
+			as: "inventory"
+		}
+	},
+	{ $unwind: "$inventory" },
+	{
+		$lookup: {
+			from: "film",
+			localField: "inventory.film_id",
+			foreignField: "film_id",
+			as: "film"
+		}
+	},
+	{ $unwind: "$film" },
+	{
+		$lookup: {
+			from: "staff",
+			localField: "rentals.staff_id",
+			foreignField: "staff_id",
+			as: "staff"
+		}
+	},
+	{ $unwind: "$staff" },
+	{
+		$group: {
+			_id: "$customer_id",
+			customer: {
+				$first: {
+					name: { $concat: ["$first_name", " ", "$last_name"] },
+					email: "$email"
+				}
+			},
+			films: {
+				$push: {
+					title: "$film.title",
+					rental_date: "$rentals.rental_date",
+					staff: {
+						$concat: ["$staff.first_name", " ", "$staff.last_name"]
+					}
+				}
+			}
+		}
+	},
+	{
+		$sort: { "customer.name": 1 }
+	},
+	{
+		$project: {
+			_id: 0,
+			customer: 1,
+			films: 1
+		}
+	},
+	{ $sort: { first_name: 1 } },
+	{ $limit: 20 }
+]).forEach(doc => printjson(doc));
+
+//para teste
+db.rental.dropIndexes();
+db.inventory.dropIndexes();
+db.film.dropIndexes();
+db.staff.dropIndexes();
