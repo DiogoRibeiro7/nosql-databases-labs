88ec472d629718a1dee85a312e5cd3fb30765f73
João Rego <116601075+joaorego01@users.noreply.github.com>
2026-01-21 18:59:03 +0000

    feat: implements average duration by route query

--- group-work/group_01/project/queries/15_average_flight_duration_by_route.mongosh.js
+++ group-work/group_01/project/queries/15_average_flight_duration_by_route.mongosh.js
@@ -3,3 +3,36 @@
 // Usage: mongosh queries/15_average_flight_duration_by_route.mongosh.js
 
 db = db.getSiblingDB("group_01_flight_management_system_final");
+print("Average flight duration by route:");
+
+db.flights.aggregate([
+  {
+    $match: {
+      departureTime: { $exists: true },
+      arrivalTime: { $exists: true }
+    }
+  },
+  {
+    $addFields: {
+      durationMinutes: {
+        $divide: [
+          { $subtract: ["$arrivalTime", "$departureTime"] },
+          60000
+        ]
+      }
+    }
+  },
+  {
+    $group: {
+      _id: {
+        departureAirportIdent: "$departureAirportIdent",
+        arrivalAirportIdent: "$arrivalAirportIdent"
+      },
+      averageDurationMinutes: { $avg: "$durationMinutes" }
+    }
+  },
+  {
+    $sort: { averageDurationMinutes: 1 }
+  }
+])
+.forEach((doc) => printjson(doc));
