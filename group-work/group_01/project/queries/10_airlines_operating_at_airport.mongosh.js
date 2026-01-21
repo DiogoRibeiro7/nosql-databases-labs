ca96e3b27d22b4dadcd19c96dbe355b9afc95de4
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-01-21 17:18:59 +0000

    feat: implement airlines operating at airport query

--- group-work/group_01/project/queries/10_airlines_operating_at_airport.mongosh.js
+++ group-work/group_01/project/queries/10_airlines_operating_at_airport.mongosh.js
@@ -3,3 +3,44 @@
 // Usage: mongosh queries/10_airlines_operating_at_airport.mongosh.js
 
 db = db.getSiblingDB("group_01_flight_management_system_final");
+print("Airlines operating at airport:");
+
+// Specific airport to filter
+const airportIdent = "NWWM";
+
+db.flights.aggregate([
+  {
+    $match: {
+      $or: [
+        { departureAirportIdent: airportIdent },
+        { arrivalAirportIdent: airportIdent }
+      ]
+    }
+  },
+  {
+    $group: {
+      _id: "$airlineIata",
+      flightsCount: { $sum: 1 }
+    }
+  },
+  {
+    $lookup: {
+      from: "airlines",
+      localField: "_id",
+      foreignField: "iata",
+      as: "airline"
+    }
+  },
+  {
+    $unwind: "$airline"
+  },
+  {
+    $project: {
+      _id: 0,
+      name: "$airline.name",
+      iata: "$airline.iata",
+      flightsCount: 1
+    }
+  },
+  { $sort: { flightsCount: -1 } }
+]).forEach((doc) => printjson(doc));
