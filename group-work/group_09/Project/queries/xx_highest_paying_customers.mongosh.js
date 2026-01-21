a0b86b0979dda4907a397203e460b65a02df2679
P <194640994+escolaP@users.noreply.github.com>
2026-01-21 18:23:24 +0000

    top clientes por gasto total

---
+++ group-work/group_09/Project/queries/xx_highest_paying_customers.mongosh.js
@@ -0,0 +1,33 @@
+print("Top 20 clientes por gasto total:");
+db.payment.aggregate([
+	{
+		$group: {
+			_id: "$customer_id",
+			totalPaid: { $sum: { $toDouble: "$amount" } }, //"toDouble" é para converter a string para float (senão o "sum" não funciona)
+			payments: { $sum: 1 },
+			lastPayment: { $max: "$payment_date" }
+		}
+	},
+	{ $sort: { totalPaid: -1 } },
+	{ $limit: 20 },
+	{
+		$lookup: {
+			from: "customer",
+			localField: "_id",
+			foreignField: "customer_id",
+			as: "customer"
+		}
+	},
+	{ $unwind: { path: "$customer", preserveNullAndEmptyArrays: true } },
+	{
+		$project: {
+			customer_id: "$_id",
+			name: { $concat: ["$customer.first_name", " ", "$customer.last_name"] },
+			email: "$customer.email",
+			totalPaid: 1,
+			payments: 1,
+			lastPayment: 1,
+			_id: 0
+		}
+	}
+]).forEach(doc => printjson(doc));
