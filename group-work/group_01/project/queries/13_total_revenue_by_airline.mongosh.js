0ef68fe91d1c3ef14d349ecf784d3d85923fa104
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-01-21 18:39:56 +0000

    feat: implement total revenue by airline

--- group-work/group_01/project/queries/13_total_revenue_by_airline.mongosh.js
+++ group-work/group_01/project/queries/13_total_revenue_by_airline.mongosh.js
@@ -3,3 +3,38 @@
 // Usage: mongosh queries/13_total_revenue_by_airline.mongosh.js
 
 db = db.getSiblingDB("group_01_flight_management_system_final");
+print("Total revenue by airline:");
+
+const airlineIata = "UA";
+
+db.flights
+  .aggregate([
+    {
+      $match: {
+        airlineIata: airlineIata
+      }
+    },
+    {
+      $lookup: {
+        from: "reservations",
+        localField: "flightNumber",
+        foreignField: "flightNumber",
+        as: "reservations"
+      }
+    },
+    {
+      $unwind: "$reservations"
+    },
+    {
+      $match: {
+        "reservations.status": "confirmed"
+      }
+    },
+    {
+      $group: {
+        _id: "$airlineIata",
+        totalRevenue: { $sum: "$price" }
+      }
+    }
+  ])
+  .forEach((doc) => printjson(doc));
